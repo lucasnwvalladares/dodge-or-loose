3538c9ad989b56e9c13e6471da987c76bea412ab
lucasnwvalladares <lucasnwvalladares@gmail.com>
2019-06-08 21:53:19 -0300

    Actual first commit

---
+++ Assets/InfoGamePhoton/NetworkController.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks {
+
+	// Use this for initialization
+	void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+	}
+
+    public override void OnConnectedToMaster()
+    {
+        // base.OnConnectedToMaster();
+
+        Debug.Log("Connected to " + PhotonNetwork.CloudRegion + " server!");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
